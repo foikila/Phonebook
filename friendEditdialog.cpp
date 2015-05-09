510a418416618cc9ac8bc3fef29aa804e196fd3d
Jonatan Karlsson <me@jonatankarlsson.se>
2015-05-09 14:04:24 +0200

    Added edit-functions

--- friendEditdialog.cpp
+++ friendEditdialog.cpp
@@ -15,7 +15,6 @@ FriendEditDialog::~FriendEditDialog()
     delete ui;
 }
 
-
 void FriendEditDialog::setFriend(Person *&f)
 {
     using std::string;
@@ -27,5 +26,13 @@ void FriendEditDialog::setFriend(Person *&f)
     this->ui->e_birthYear->setText(QString::number(this->f->getBirthYear()));
     this->ui->e_phoneNumber->setText(QString::fromStdString(this->f->getPhoneNumber()));
     this->ui->e_homeNumber->setText(QString::fromStdString(this->f->getHomeNumber()));
+}
 
+void FriendEditDialog::on_buttonBox_accepted()
+{
+    this->f->setName(this->ui->e_name->text().toStdString());
+    this->f->setEmail(this->ui->e_email->text().toStdString());
+    this->f->setBirthYear(this->ui->e_birthYear->text().toInt());
+    this->f->setPhoneNumber(this->ui->e_phoneNumber->text().toStdString());
+    this->f->setHomeNumber(this->ui->e_homeNumber->text().toStdString());
 }
--- friendEditdialog.h
+++ friendEditdialog.h
@@ -19,6 +19,8 @@ public:
     ~FriendEditDialog();
     void setFriend(Person *&f);
 
+private slots:
+    void on_buttonBox_accepted();
 };
 
 #endif // FRIENDEDITDIALOG_H
